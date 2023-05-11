8ceead7123f84d48a79916b909293e3ec8d470a9
fadziie <knxtfdzw@gmail.com>
2023-05-11 19:57:13 +0200

    images optimized,changes to javascript

--- lesson2/scripts/index.js
+++ lesson2/scripts/index.js
@@ -3,3 +3,8 @@ let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-d
 let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 let dateTime = `Last Updated: ${fullDate} ${time}`;
 document.getElementById("lastModified").innerHTML = dateTime;
+// Get current year
+var currentYear = new Date().getFullYear();
+  
+// Set current year in HTML
+document.getElementById("currentYear").textContent = currentYear;
--- lesson3/scripts/index.js
+++ lesson3/scripts/index.js
@@ -3,6 +3,8 @@ let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-d
 let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 let dateTime = `Last Updated: ${fullDate} ${time}`;
 document.getElementById("lastModified").innerHTML = dateTime;
+var currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 menuToggle.addEventListener('click', function() {
