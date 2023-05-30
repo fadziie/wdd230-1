65a42c96c63606e22c03e184476ae85cc76b3032
fadziie <knxtfdzw@gmail.com>
2023-05-30 21:23:31 +0200

    aspect ratio fixed

--- lesson4/chamber/scripts/windchill.js
+++ lesson4/chamber/scripts/windchill.js
@@ -5,6 +5,12 @@ const windSpeedElement = document.querySelector("#wind-speed");
 // Extract the temperature and wind speed values
 const temperature = parseFloat(temperatureElement.textContent);
 const windSpeed = parseFloat(windSpeedElement.textContent);
+
+// Calculate the wind chill factor
+function calculateWindChill(temperature, windSpeed) {
+  const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+  return windChill.toFixed(2);
+}
 // Check if values meet the specification limits
 const meetsRequirements = temperature <= 50 && windSpeed > 3.0;
 // Calculate the wind chill factor or display "N/A"
