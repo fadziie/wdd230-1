3eb8395144c852ac50b4c04463f7dfc0afb795bf
fadziie <knxtfdzw@gmail.com>
2023-07-02 22:00:50 +0200

    dynamic spotlights

--- lesson4/chamber/scripts/spotlight.js
+++ lesson4/chamber/scripts/spotlight.js
@@ -27,12 +27,10 @@ function displayBusinesses(businesses) {
       const spotlightSection = spotlightSections[index];
       const heading = spotlightSection.querySelector("h3");
       const description = spotlightSection.querySelector("p");
-      const logo = spotlightSection.querySelector(".logo");
       const phoneNumber = spotlightSection.querySelector(".phone");
   
       heading.textContent = business.companyName;
       description.textContent = business.description;
-      logo.style.backgroundImage = `url(${business.siteLogo})`;
       phoneNumber.textContent = "Phone: " + business.number;
 
       // Optional: Create a link for each business
